0dbfdfac8a8f4f66c691dd13cbfd31b4de272c9a
Jose Miguel Urcia Guevara <miguelurcia2004@gmail.com>
2025-06-16 21:49:49 -0500

    deivi qlon

---
+++ app/Livewire/Registro/ProductosCrear.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Registro;
+
+use Livewire\Component;
+
+class ProductosCrear extends Component
+{
+    public function render()
+    {
+        return view('livewire.registro.productos-crear');
+    }
+}
